f79f4968ab7e4bcdbb1bfe82b59f14514daf4575
AmaseCocoa <amase.cocoa@gmail.com>
2024-08-31 01:39:29 +0900

    add response to all

--- src/fubuki/__init__.py
+++ src/fubuki/__init__.py
@@ -3,5 +3,6 @@ from .controller import Controller
 from .route import route, get, post, ws
 from .websocket import WebSocket
 from .request import Request
+from .response import Response
 
 __all__ = ["Fubuki", "Controller", "route", "get", "post", "ws", "WebSocket", "Request"]
